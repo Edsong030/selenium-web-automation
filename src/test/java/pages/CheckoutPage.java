71e1854a7afc7cb502b272655f8b82514def0380
EDSON GOMES <edson.eg18@gmail.com>
2026-02-16 18:15:49 -0300

    Fix checkout negative flow

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class CheckoutPage {
@@ -26,45 +25,25 @@ public class CheckoutPage {
     }
 
     public void preencherDados(String nome, String sobrenome, String cep) {
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(firstName));
-
-        driver.findElement(firstName).clear();
-        driver.findElement(firstName).sendKeys(nome);
-
-        driver.findElement(lastName).clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstName)).sendKeys(nome);
         driver.findElement(lastName).sendKeys(sobrenome);
-
-        driver.findElement(postalCode).clear();
         driver.findElement(postalCode).sendKeys(cep);
-
-        wait.until(ExpectedConditions.elementToBeClickable(continueButton));
         driver.findElement(continueButton).click();
-
-        // Aguarda realmente mudar de página
-        wait.until(ExpectedConditions.visibilityOfElementLocated(finishButton));
-    }
-
-
-    public void finalizarCompra() {
-        wait.until(ExpectedConditions.elementToBeClickable(finishButton));
-        driver.findElement(finishButton).click();
-    }
-
-    public boolean compraFinalizadaComSucesso() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(successMessage));
-        return driver.findElement(successMessage).isDisplayed();
     }
 
     public void continuarSemDados() {
-        wait.until(ExpectedConditions.elementToBeClickable(continueButton));
-        driver.findElement(continueButton).click();
+        wait.until(ExpectedConditions.elementToBeClickable(continueButton)).click();
+    }
+
+    public void finalizarCompra() {
+        wait.until(ExpectedConditions.elementToBeClickable(finishButton)).click();
+    }
+
+    public boolean compraFinalizadaComSucesso() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(successMessage)).isDisplayed();
     }
 
     public boolean erroVisivel() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage));
-        return true;
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).isDisplayed();
     }
-
-
 }
