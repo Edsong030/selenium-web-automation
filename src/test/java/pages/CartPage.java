1c783fc643570c1539c0d2de5e7f1ea4af32549c
EDSON GOMES <92475786+Edsong030@users.noreply.github.com>
2026-02-17 16:19:48 -0300

    Add CartPage implementation for cart item validation and checkout button functionality

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -2,48 +2,29 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 public class CartPage {
-
     private WebDriver driver;
-    private WebDriverWait wait;
 
-    private By cartItem = By.className("cart_item");
+    // Locators
+    private By cartItems = By.cssSelector(".cart-item");
     private By checkoutButton = By.id("checkout");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public boolean produtoEstaNoCarrinho() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(cartItem));
-        return driver.findElement(cartItem).isDisplayed();
+    // Method to validate if the cart has items
+    public boolean isCartEmpty() {
+        return driver.findElements(cartItems).isEmpty();
     }
 
-    public void clicarCheckout() {
-
-        WebElement botaoCheckout = wait.until(
-                ExpectedConditions.elementToBeClickable(checkoutButton)
-        );
-
-        ((JavascriptExecutor) driver)
-                .executeScript("arguments[0].scrollIntoView(true);", botaoCheckout);
-
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+    // Method to click on the checkout button
+    public void clickCheckout() {
+        WebElement checkout = driver.findElement(checkoutButton);
+        if (checkout.isDisplayed() && checkout.isEnabled()) {
+            checkout.click();
         }
-
-        ((JavascriptExecutor) driver)
-                .executeScript("arguments[0].click();", botaoCheckout);
-
-        wait.until(ExpectedConditions.urlContains("checkout-step-one"));
     }
 }
